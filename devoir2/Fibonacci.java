4a736c4d0d4a09d991f85d79ab42a78bb5858939
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-06-16 03:13:58 -0400

    Ajout d'une class extend thread qui represente le fil enfant et qui calcule la suite de fib.

--- devoir2/Fibonacci.java
+++ devoir2/Fibonacci.java
@@ -22,7 +22,47 @@ public class Fibonacci {
         }
 
         System.out.println("le nombre est:" + nombre);
+        int[] sequence = new int[nombre];
+
+        // lance un fil
+        ThreadFibonacci threadFibonacci = new ThreadFibonacci(nombre, sequence);
+        threadFibonacci.start();
+
+        try {
+            threadFibonacci.join(); // Attend la fin du fil enfant
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Afficher la séquence de Fibonacci recue par l'enfant
+        System.out.println("Séquence de Fibonacci:");
+        for (int i = 0; i < nombre; i++) {
+            System.out.println(sequence[i]);
+        }
 
     }
 
+}
+
+class ThreadFibonacci extends Thread { // fil enfant
+    // espace memoire partagé par les fils
+    private int[] sequence;
+    private int n;
+
+    public ThreadFibonacci(int n, int[] sequence) { // recoit le tableau a remplir et le nombre de fibonnaci n
+        this.n = n;
+        this.sequence = sequence;
+    }
+
+    public void run() { // rempli le tableau
+
+        if (n >= 1)
+            sequence[0] = 0;
+        if (n >= 2)
+            sequence[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+    }
 }
