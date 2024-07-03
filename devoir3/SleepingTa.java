52a94f2532025886c5931396499a754a3a9b7da9
ChadaBendriss <113207156+ChadaBendriss@users.noreply.github.com>
2024-07-03 10:39:27 -0400

    Added semaphores and constructors for TA and Student

--- devoir3/SleepingTa.java
+++ devoir3/SleepingTa.java
@@ -1,6 +1,7 @@
 package devoir3;
 
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 /**
  * Devoir 3 The Sleeping Ta
@@ -27,12 +28,49 @@ public class SleepingTa {
 }
 
 class Student extends Thread {
+
+
+    private int id;
+    private static Semaphore chairsMutex = new Semaphore(1);
+    private static Semaphore students = new Semaphore(0);
+    private static Semaphore ta = new Semaphore(0);
+    private static int waitingStudents = 0;
+    private static final int NUM_CHAIRS = 3;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+
+    
+
     public void run() {
         System.out.println("Student");
       }
 }
 
 class TA extends Thread {
+
+
+    private int port = 0;  // On commence au port 0
+    private int charge = 0;  // Aucune charge au debut
+    private int nombreTours;  // nombres de tours a executer
+    // Semaphores
+    public Semaphore semEmbPort0;  // Semaphore pour embarquer au port 0
+    public Semaphore semEmbPort1;  // Semaphore pour embarquer au port 1
+    public Semaphore semDebarque;  // Semaphore pour debarquer 
+    public Semaphore semDepart;    // Semaphore pour le depart du traversier
+
+    public TA(int prt, int nbtours) {
+        this.port = prt;
+        semEmbPort0 = new Semaphore(0, true);
+        semEmbPort1 = new Semaphore(0, true);
+        semDebarque = new Semaphore(0, true);
+        semDepart = new Semaphore(0, true);
+        nombreTours = nbtours;
+    }
+
+
     public void run() {
         System.out.println("TA");
       }
