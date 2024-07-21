fc97d95fde76d03a0e56df68bbfddb229d40f1aa
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-07-20 20:10:17 -0400

    q2 de base

---
+++ devoir4/question2.c
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------
+Fichier: question2.c
+
+Nom: Chloe Al-Frenn, Chada Bendriss
+Numero d'etudiant:  300211508, 300266679
+
+Description: Ce programme contient le code pour l'implémentation
+d'un programme qui implémente les algorithmes de remplacement 
+de pages FIFO et LRU présentés dans le module 8.
+
+-------------------------------------------------------------*/
